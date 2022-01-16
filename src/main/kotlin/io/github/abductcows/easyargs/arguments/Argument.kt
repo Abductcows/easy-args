2afeb9729e1a721d6a5cce96b57b00c09a4fc1e4
george <abductcows@gmail.com>
2022-01-17 00:02:32 +0200

    .

--- src/main/kotlin/io/github/abductcows/easyargs/arguments/Argument.kt
+++ src/main/kotlin/io/github/abductcows/easyargs/arguments/Argument.kt
@@ -2,38 +2,45 @@
 
 package io.github.abductcows.easyargs.arguments
 
-import com.sun.org.apache.xpath.internal.Arg
-
-class Argument @JvmOverloads constructor(
+class Argument internal constructor(
+    shortName: String = "",
+    longName: String = "",
+    needsValue: Boolean = false,
+    description: String = ""
+) {
     /**
      * Short name of the argument e.g. -p or -v
      */
-    var shortName: String = "",
+    var shortName: String = shortName
+        private set
+
     /**
      * Full name of the argument e.g. --port or --version
      */
-    var longName: String = "",
+    var longName: String = longName
+        private set
+
     /**
      * Whether the argument needs an additional value e.g. --port 8080
      */
-    var needsValue: Boolean = false,
+    var needsValue: Boolean = needsValue
+        private set
+
     /**
      * Quick summary of the argument's effect
      */
-    var description: String = "",
-) {
+    var description: String = description
+        private set
 
     companion object {
+        @JvmStatic
+        fun withShortName(shortName: String) = Builder().apply { shortName(shortName) }
 
-        fun builder() = Builder.instance
+        @JvmStatic
+        fun withLongName(longName: String) = Builder().apply { longName(longName) }
     }
 
-    class Builder private constructor() {
-
-        companion object {
-            val instance = Builder()
-        }
-
+    class Builder {
         private var current: Argument = Argument()
 
         fun shortName(shortName: String) = apply { current.shortName = shortName }
@@ -41,6 +48,10 @@ class Argument @JvmOverloads constructor(
         fun needsValue(needsValue: Boolean) = apply { current.needsValue = needsValue }
         fun description(description: String) = apply { current.description = description }
 
-        fun build(): Argument = current.also { current = Argument() }
+        fun build(): Argument = current.also { reset() }
+
+        private fun reset() {
+            current = Argument()
+        }
     }
 }
